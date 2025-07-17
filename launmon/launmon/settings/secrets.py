8e8778f2a5dad13e2721cc772abce83f7e0af467
matt-robbins <matthew.robbins@gmail.com>
2025-07-17 14:01:36 -0400

    Delete launmon/launmon/settings/secrets.py

--- launmon/launmon/settings/secrets.py
+++
@@ -1,7 +0,0 @@
-SECRET_KEY = 'tfamt$5c@(_efm_v9#7-5uwwx7e&*d=h52+*s8+*l22kyzbi&z'
-
-EMAIL_HOST = "smtp.porkbun.com"
-EMAIL_HOST_USER = "admin@spintracker.app"
-EMAIL_HOST_PASSWORD = "Gu1t4rS0ld3r!"
-EMAIL_FROM_USER = "admin@spintracker.app"
-DEFAULT_FROM_EMAIL = "admin@spintracker.app"
